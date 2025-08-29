4d4f49fe370d10ab928b56b09c93dd2489c1c87d
Sakshi <sakshigupta8624@gmail.com>
2025-08-29 13:32:16 +0530

    added dort option

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -5,6 +5,7 @@ import FoodItem from '../FoodItem/FoodItem';
 
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
+  const [sortOption, setSortOption] = useState("");
   const [filterType, setFilterType] = useState('all'); // all | veg | non-veg
 
   const handleToggle = (type) => {
@@ -19,12 +20,22 @@ const FoodDisplay = ({ category }) => {
       (filterType === 'non-veg' && item.type === 'nonveg');
     return matchCategory && matchType;
   });
+    if (sortOption === "alpha-asc") {
+  filteredFoodList.sort((a, b) => a.name.localeCompare(b.name));
+} else if (sortOption === "alpha-desc") {
+  filteredFoodList.sort((a, b) => b.name.localeCompare(a.name));
+} else if (sortOption === "price-asc") {
+  filteredFoodList.sort((a, b) => a.price - b.price);
+} else if (sortOption === "price-desc") {
+  filteredFoodList.sort((a, b) => b.price - a.price);
+}
 
   return (
     <div className='food-display' id='food-display'>
       <h2>Top Dishes Near You</h2>
       
-      {/* Toggle Buttons */}
+      <div className="contt">
+        {/* Toggle Buttons */}
       <div className="filter-toggle">
         <button
           className={filterType === 'all' ? 'active' : ''}
@@ -44,6 +55,21 @@ const FoodDisplay = ({ category }) => {
         >
           Non-Veg
         </button>
+      </div>
+      <div className="sort-container">
+        <label htmlFor="sort">Sort by: </label>
+        <select
+          id="sort"
+          value={sortOption}
+          onChange={(e) => setSortOption(e.target.value)}
+        >
+          <option value="">None</option>
+          <option value="alpha-asc">Alphabet (A → Z)</option>
+          <option value="alpha-desc">Alphabet (Z → A)</option>
+          <option value="price-asc">Price (Low → High)</option>
+          <option value="price-desc">Price (High → Low)</option>
+        </select>
+      </div>
       </div>
 
       <div className='food-display-list'>
